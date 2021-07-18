7ffb1a95b57cc8210e2683d06db6d139b7441b20
Mohd Araman <mohdarman005@gmail.com>
2021-07-18 10:04:06 +0530

    some

---
+++ todo_incre/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function App() {
+  const [num, setNum] = useState(0);
+  const incre = () => {
+    setNum(num + 1);
+  };
+  const decre = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    } else {
+      alert("Number less then 0 not valid");
+    }
+  };
+  return (
+    <div className="main_div">
+      <div className="center_div">
+        <h3>{num}</h3>
+        <button onClick={incre}>+</button>
+        <button onClick={decre}>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
